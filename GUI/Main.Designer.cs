437a676a69b17b45c9bfd4a952a271f67d4fe724
Takasakiii <lucasmc2709@live.com>
2018-11-06 21:50:12 -0200

    Editar Imgs Ta top (foda-se gringo .com.br)

--- GUI/Main.Designer.cs
+++ GUI/Main.Designer.cs
@@ -35,20 +35,29 @@
             this.localArquivoTXT = new System.Windows.Forms.TextBox();
             this.NomeArquivoLB = new System.Windows.Forms.Label();
             this.ImagemLoaderFD = new System.Windows.Forms.OpenFileDialog();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.salvarBT = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.TamanhoCB = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.CarregadorGB.SuspendLayout();
-            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // EditorPn
             // 
+            this.EditorPn.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.EditorPn.AutoScroll = true;
             this.EditorPn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.EditorPn.Location = new System.Drawing.Point(6, 19);
+            this.EditorPn.Location = new System.Drawing.Point(414, 12);
             this.EditorPn.Name = "EditorPn";
-            this.EditorPn.Size = new System.Drawing.Size(484, 575);
+            this.EditorPn.Size = new System.Drawing.Size(0, 0);
             this.EditorPn.TabIndex = 0;
+            this.EditorPn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.EditorPn_MouseDown);
+            this.EditorPn.MouseMove += new System.Windows.Forms.MouseEventHandler(this.EditorPn_MouseMove);
             // 
             // CarregadorGB
             // 
@@ -57,7 +66,7 @@
             this.CarregadorGB.Controls.Add(this.NomeArquivoLB);
             this.CarregadorGB.Location = new System.Drawing.Point(12, 12);
             this.CarregadorGB.Name = "CarregadorGB";
-            this.CarregadorGB.Size = new System.Drawing.Size(386, 49);
+            this.CarregadorGB.Size = new System.Drawing.Size(386, 51);
             this.CarregadorGB.TabIndex = 1;
             this.CarregadorGB.TabStop = false;
             this.CarregadorGB.Text = "Adicionar Imagem:";
@@ -95,42 +104,86 @@
             this.ImagemLoaderFD.Title = "Selecione a imagem de origem:";
             this.ImagemLoaderFD.FileOk += new System.ComponentModel.CancelEventHandler(this.ImagemLoaderFD_FileOk);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.EditorPn);
-            this.groupBox1.Location = new System.Drawing.Point(404, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(496, 600);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Visualizar e selecionar área de ação: ";
-            // 
             // groupBox2
             // 
-            this.groupBox2.Location = new System.Drawing.Point(12, 67);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.TamanhoCB);
+            this.groupBox2.Controls.Add(this.salvarBT);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 130);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.Size = new System.Drawing.Size(386, 92);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Text = "Controles:";
+            // 
+            // salvarBT
+            // 
+            this.salvarBT.Location = new System.Drawing.Point(90, 20);
+            this.salvarBT.Name = "salvarBT";
+            this.salvarBT.Size = new System.Drawing.Size(110, 23);
+            this.salvarBT.TabIndex = 1;
+            this.salvarBT.Text = "Save and Convert";
+            this.salvarBT.UseVisualStyleBackColor = true;
+            this.salvarBT.Click += new System.EventHandler(this.SalvarBT_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Corrigir";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(12, 69);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(386, 55);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Local do DeepCreamPy:";
+            // 
+            // TamanhoCB
+            // 
+            this.TamanhoCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TamanhoCB.Location = new System.Drawing.Point(90, 49);
+            this.TamanhoCB.Name = "TamanhoCB";
+            this.TamanhoCB.Size = new System.Drawing.Size(121, 21);
+            this.TamanhoCB.TabIndex = 2;
+            this.TamanhoCB.SelectedIndexChanged += new System.EventHandler(this.TamanhoCB_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tamanho:";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(912, 624);
+            this.ClientSize = new System.Drawing.Size(413, 232);
+            this.Controls.Add(this.EditorPn);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.CarregadorGB);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DeepCreamPy GUI";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.CarregadorGB.ResumeLayout(false);
             this.CarregadorGB.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -143,8 +196,12 @@
         private System.Windows.Forms.TextBox localArquivoTXT;
         private System.Windows.Forms.Label NomeArquivoLB;
         private System.Windows.Forms.OpenFileDialog ImagemLoaderFD;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button salvarBT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox TamanhoCB;
     }
 }
 
--- GUI/Main.cs
+++ GUI/Main.cs
@@ -12,9 +12,24 @@ namespace GUI
 {
     public partial class Main : Form
     {
+        private Form preset = new Form();
+        private Point atual = new Point();
+        private Point antigo = new Point();
+        private Pen caneta = new Pen(Color.FromArgb(0, 255, 0), 5);
+        private Graphics editar;
+
         public Main()
         {
             InitializeComponent();
+            preset.Size = Size;
+            List<int> temp = new List<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                temp.Add(i);
+            }
+
+            TamanhoCB.DataSource = temp;
+            TamanhoCB.SelectedIndex = 4;
         }
 
         private void CarregarArquivoBT_Click(object sender, EventArgs e)
@@ -24,9 +39,51 @@ namespace GUI
 
         private void ImagemLoaderFD_FileOk(object sender, CancelEventArgs e)
         {
+            Size = preset.Size;
             localArquivoTXT.Text = ImagemLoaderFD.FileName;
             Image imagem = Image.FromFile(localArquivoTXT.Text);
-            imagem.GetFrameCount
+            EditorPn.Size = imagem.Size;
+            Width += EditorPn.Width + 10;
+            if (imagem.Height > Height)
+            {
+                Height = imagem.Height + 60;
+            }
+            editar = EditorPn.CreateGraphics();
+            EditorPn.BackgroundImage = imagem;
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SalvarBT_Click(object sender, EventArgs e)
+        {
+            //Bitmap 
+        }
+
+        private void EditorPn_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                atual = e.Location;
+                editar.DrawLine(caneta, antigo, atual);
+                antigo = atual;
+            }
+        }
+
+        private void EditorPn_MouseDown(object sender, MouseEventArgs e)
+        {
+            antigo = e.Location;
+        }
+
+        private void TamanhoCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(TamanhoCB.SelectedItem != null)
+            {
+                caneta.Width = Convert.ToInt32(TamanhoCB.SelectedItem);
+            }
+            
         }
     }
 }
